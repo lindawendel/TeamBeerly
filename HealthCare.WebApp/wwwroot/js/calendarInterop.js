c871cb27102e1d8dbfbe87c9311aedf8ec1d8615
Philip Högfeldt <112342813+Philiphogfeldt@users.noreply.github.com>
2024-01-02 10:58:35 +0100

    Implemented the base for calendar in ViewAppointments using the FullCalendar plugin

---
+++ HealthCare.WebApp/wwwroot/js/calendarInterop.js
@@ -0,0 +1,13 @@
+﻿window.initializeCalendar = () => {
+    console.log("Initializing Calendar");
+    var calendarEl = document.getElementById('calendar');
+    if (calendarEl) {
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth'
+        });
+        calendar.render();
+        console.log("Calendar rendered");
+    } else {
+        console.error("Calendar element not found");
+    }
+};
