fe7a29f5da554d59b0a9bbfce7b57a7b6e9a7f8e
Linda Wendel <linda.m.wendel@gmail.com>
2024-01-05 12:13:20 +0100

    added test-file

---
+++ HealthCareTests/PatientProfileTests.cs
@@ -0,0 +1,38 @@
+﻿using HealthCare.Core.Data;
+using HealthCare.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCareTests
+{
+    public class PatientProfileTests : IDisposable
+    {
+        private readonly HealthCareContext _context;
+
+        public PatientProfileTests()
+        {
+            var options = new DbContextOptionsBuilder<HealthCareContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            _context = new HealthCareContext(options);
+        }
+
+        [Fact]
+
+
+        public void Dispose()
+        {
+            // Dispose of the context after each test
+            _context.Dispose();
+        }
+
+
+
+
+    }
+}
