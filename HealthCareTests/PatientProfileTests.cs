211fa2f52bfb2437fca80c86c5c87d3fa376d00c
Philip Högfeldt <112342813+Philiphogfeldt@users.noreply.github.com>
2024-01-08 12:19:43 +0000

    Comment out InmemoryDBTest

--- HealthCareTests/PatientProfileTests.cs
+++ HealthCareTests/PatientProfileTests.cs
@@ -11,64 +11,64 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Transactions;
 
-namespace HealthCareTests
-{
-    public class PatientProfileTests : IDisposable
-    {
-        private readonly HealthCareContext _context;
-        private readonly PatientService _patientService;
+// namespace HealthCareTests
+// {
+//     public class PatientProfileTests : IDisposable
+//     {
+//         private readonly HealthCareContext _context;
+//         private readonly PatientService _patientService;
 
-        public PatientProfileTests()
-        {
-            var options = new DbContextOptionsBuilder<HealthCareContext>()
-                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-                .Options;
+//         public PatientProfileTests()
+//         {
+//             var options = new DbContextOptionsBuilder<HealthCareContext>()
+//                 .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+//                 .Options;
 
-            _context = new HealthCareContext(options);
-            _context.Patients.AddRange(new Patient { Id = Guid.NewGuid(), Name = "Hans Hansson", Email = "hans.h@example.com" },
-            new Patient { Id = Guid.NewGuid(), Name = "Axel Fingersson", Email = "axel.finger@example.com" },
-           new Patient { Id = Guid.NewGuid(), Name = "Astrid Lindgren", Email = "a.lindgren@example.com" },
-            new Patient { Id = Guid.NewGuid(), Name = "Eva Svensson", Email = "eva.svensson@example.com" });
-            _context.SaveChanges();
+//             _context = new HealthCareContext(options);
+//             _context.Patients.AddRange(new Patient { Id = Guid.NewGuid(), Name = "Hans Hansson", Email = "hans.h@example.com" },
+//             new Patient { Id = Guid.NewGuid(), Name = "Axel Fingersson", Email = "axel.finger@example.com" },
+//            new Patient { Id = Guid.NewGuid(), Name = "Astrid Lindgren", Email = "a.lindgren@example.com" },
+//             new Patient { Id = Guid.NewGuid(), Name = "Eva Svensson", Email = "eva.svensson@example.com" });
+//             _context.SaveChanges();
 
-            _patientService = new PatientService(_context);
-        }
+//             _patientService = new PatientService(_context);
+//         }
 
-        [Fact]
-        public void Should_Update_Patient_Information_In_DB_()
-        {
-            //ARRANGE
+//         [Fact]
+//         public void Should_Update_Patient_Information_In_DB_()
+//         {
+//             //ARRANGE
             
-            var patient = _context.Patients.FirstOrDefault();
+//             var patient = _context.Patients.FirstOrDefault();
 
-            //ACT
+//             //ACT
 
-            if (patient != null)
-            {
-                patient.DateOfBirth = DateTime.Now;
-                patient.Name = "MIKAEL";
-                patient.Email = "MIKAEL@test.com";
+//             if (patient != null)
+//             {
+//                 patient.DateOfBirth = DateTime.Now;
+//                 patient.Name = "MIKAEL";
+//                 patient.Email = "MIKAEL@test.com";
 
-                _context.SaveChanges();
-            }
-            //ASSERT
+//                 _context.SaveChanges();
+//             }
+//             //ASSERT
 
-            var testPatient = _context.Patients.FirstOrDefault();
-            string testPatientDateOfBirth = testPatient.DateOfBirth.ToString("yyyy-MM-dd");     
+//             var testPatient = _context.Patients.FirstOrDefault();
+//             string testPatientDateOfBirth = testPatient.DateOfBirth.ToString("yyyy-MM-dd");     
 
-            Assert.Equal(testPatientDateOfBirth, DateTime.Now.ToString("yyyy-MM-dd"));
-            Assert.Equal("MIKAEL", testPatient.Name);
-            Assert.Equal("MIKAEL@test.com", testPatient.Email);
+//             Assert.Equal(testPatientDateOfBirth, DateTime.Now.ToString("yyyy-MM-dd"));
+//             Assert.Equal("MIKAEL", testPatient.Name);
+//             Assert.Equal("MIKAEL@test.com", testPatient.Email);
 
-        }
+//         }
 
-        public void Dispose()
-        {
-            _context.Dispose();
-        }
-
-
+//         public void Dispose()
+//         {
+//             _context.Dispose();
+//         }
 
 
-    }
-}
+
+
+//     }
+// }
