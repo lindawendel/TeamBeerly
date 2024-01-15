01a8769c8f02d5b46aba72dd7a3cc8dc53c3ec78
fredrikgreco <112250686+fredrikgreco@users.noreply.github.com>
2024-01-15 14:49:23 +0100

    added Service for Caregiver

---
+++ HealthCare.Core/CaregiverService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HealthCare.Core.Data;
+using HealthCare.Core.Models;
+
+namespace HealthCare.Core
+{
+    public class CaregiverService
+    {
+        private readonly HealthCareContext _dbContext;
+
+        public CaregiverService(HealthCareContext dbContext)
+        {
+            _dbContext = dbContext;
+
+            // Ensure the database is created
+            _dbContext.Database.EnsureCreated();
+        }
+
+        public IEnumerable<Caregiver> GetCaregivers()
+        {
+            return _dbContext.Caregivers.ToList();
+        }
+
+
+    }
+}
+
+
+
